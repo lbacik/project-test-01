0dcf57d6f2ea067ebc4f5215538ea06550419d23
Łukasz Bacik <mail@luka.sh>
2021-01-23 11:04:36 +0100

    updated dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,7 +5,13 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Repository\ItemsRepository;
+use App\Service\RedisService;
+use App\Service\UploadService;
+use stdClass;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
@@ -17,15 +23,61 @@ class DashboardController extends AbstractController
     ) {}
 
     #[Route('/', name: 'dashboard')]
-    public function index(ItemsRepository $itemRepository): Response
-    {
+    public function index(
+        Request $request,
+        UploadService $uploadService,
+        ItemsRepository $itemRepository,
+        RedisService $redisService
+    ): Response {
+
+        $itemsInDb = $itemRepository->findAll();
+
+        $form = $this->uploadForm();
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $uploadService->uploadToRedis($itemsInDb);
+        }
+
+        $itemsInRedis = $this->mapToObjects(
+            $redisService->getAll()
+        );
+
         return new Response(
             $this->twig->render(
                 'dashboard/index.html.twig',
                 [
-                    'itemsInDb' => $itemRepository->findAll(),
+                    'itemsInDb' => $itemsInDb,
+                    'itemsInRedis' => $itemsInRedis,
+                    'UploadForm' => $form->createView(),
                 ]
             ),
         );
+    }
+
+    private function uploadForm(): FormInterface
+    {
+        return $this->createFormBuilder(
+                [],
+                [
+                    'action' => $this->generateUrl('dashboard'),
+                ]
+            )
+            ->add('Upload', SubmitType::class)
+            ->getForm();
+    }
+
+    private function mapToObjects(array $associativeArray): array
+    {
+        $result = [];
+        foreach($associativeArray as $key => $value) {
+
+            $obj = new stdClass();
+
+            $obj->name = $key;
+            $obj->value = $value;
+
+            $result[] = $obj;
+        }
+        return $result;
     }
 }
